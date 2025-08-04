cbf299d0c6c96647b5b27fe528780c2f80a4743d
LeeZi <35288214+xianyu21@users.noreply.github.com>
2025-08-04 09:24:06 +0800

    feat: 初始化项目基础配置和文件结构

---
+++ src/pages/about/about.vue
@@ -0,0 +1,65 @@
+<route lang="jsonc" type="page">
+{
+  "layout": "tabbar",
+  "style": {
+    "navigationBarTitleText": "关于"
+  }
+}
+</route>
+
+<script lang="ts" setup>
+import RequestComp from './components/request.vue'
+
+// 奇怪：同样的代码放在 vue 里面不会校验到错误，放在 .ts 文件里面会校验到错误
+// const testOxlint = (name: string) => {
+//   console.log('oxlint')
+// }
+// testOxlint('oxlint')
+console.log('about')
+
+function gotoAlova() {
+  uni.navigateTo({
+    url: '/pages/about/alova',
+  })
+}
+function gotoVueQuery() {
+  uni.navigateTo({
+    url: '/pages/about/vue-query',
+  })
+}
+</script>
+
+<template>
+  <view>
+    <view class="mt-8 text-center text-xl text-gray-400">
+      组件使用、请求调用、unocss
+    </view>
+    <RequestComp />
+    <view class="mb-6 h-1px bg-#eee" />
+    <view class="text-center">
+      <button type="primary" size="mini" class="w-160px" @click="gotoAlova">
+        前往 alova 示例页面
+      </button>
+    </view>
+    <view class="text-center">
+      <button type="primary" size="mini" class="w-160px" @click="gotoVueQuery">
+        vue-query 示例页面
+      </button>
+    </view>
+    <view class="mt-6 text-center text-sm">
+      <view class="inline-block w-80% text-gray-400">
+        为了方便脚手架动态生成不同UI模板，本页的按钮统一使用UI库无关的原生button
+      </view>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.test-css {
+  // 16rpx=>0.5rem
+  padding-bottom: 16rpx;
+  // mt-4=>1rem=>16px;
+  margin-top: 16px;
+  text-align: center;
+}
+</style>
---
+++ src/pages/about/vue-query.vue
@@ -0,0 +1,53 @@
+<route lang="jsonc" type="page">
+{
+  "layout": "default",
+  "style": {
+    "navigationBarTitleText": "Vue Query 请求演示"
+  }
+}
+</route>
+
+<script lang="ts" setup>
+import { useQuery } from '@tanstack/vue-query'
+import { foo } from '@/api/foo'
+import { getFooQueryOptions } from '@/service/index/vue-query'
+
+// 简单使用
+onShow(async () => {
+  const res = await foo()
+  console.log('res: ', res)
+})
+
+// vue-query 版
+const {
+  data,
+  error,
+  isLoading: loading,
+  refetch: send,
+} = useQuery(getFooQueryOptions('菲鸽-vue-query'))
+</script>
+
+<template>
+  <view class="p-6 text-center">
+    <button type="primary" size="mini" class="my-6 w-160px" @click="send">
+      发送请求
+    </button>
+    <view class="h-16">
+      <view v-if="loading">
+        loading...
+      </view>
+      <block v-else>
+        <view class="text-xl">
+          请求数据如下
+        </view>
+        <view class="text-green leading-8">
+          {{ JSON.stringify(data) }}
+        </view>
+      </block>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
